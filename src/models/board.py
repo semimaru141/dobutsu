dfce17c912132b783cf4422486cb6467f77db44a
semimaru141 <chiharu.ishii@keio.jp>
2023-04-20 15:01:12 +0900

    盤面の反転処理を作成

--- src/models/board.py
+++ src/models/board.py
@@ -57,7 +57,10 @@ class State:
         str += "]\n\n"
 
         return str
-
+    
+    def turn(self):
+        board, captured = self.getState()
+        return State(board.turn(), captured.turn())
 
 class Board:
     def __init__(self, board: list[int]):
@@ -66,17 +69,28 @@ class Board:
     def copy(self) -> 'Board':
         return Board(self._board.copy())
 
+    # 該当のPlaceについて、Pieceの情報を与える
     def getPiece(self, place: Place) -> Piece:
         return self._board[place]
 
+    # 該当のPlaceにPieceを設置する
     def setPiece(self, place: Place, piece: Piece):
         self._board[place] = piece
 
+    # 出力用
     def getStr(self, place: Place) -> str:
         return b_char[self._board[place]]
     
+    # 駒が配置可能なPlaceを取得する
     def getEmptyPlaces(self) -> list[Place]:
         return [place for place in range(RANGE_OF_BOARD) if self._board[place] == EMPTY]
+    
+    # 先手後手入れ替える
+    def turn(self):
+        # 1. 配列を反転する
+        # 2. OPとMYの駒を反転させる
+        new_board = [turnPiece(piece) for piece in self._board[::-1]]
+        return Board(new_board)
     
 class Captured:
     def __init__(self, captured: list[int]):
@@ -85,30 +99,41 @@ class Captured:
     def copy(self) -> 'Captured':
         return Captured(self._captured.copy())
     
+    # Pieceを取得する。該当のPieceの個数を増やす
     def takePiece(self, piece: Piece):
         if(piece == OP_LION_NUM): return
         index = parsePiece(piece)
         self._captured[index] += 1
-    
+
+    # Pieceを使用する。該当するPieceの個数を減らす 
     def usePiece(self, piece: Piece):
         index = parsePiece(piece)
         self._captured[index] -= 1
-        print (self._captured)
 
+    # 出力用
     def getMyStr(self) -> str:
         ret = ''
         for i in range(3):
             ret += c_char[i]*self._captured[i]
         return ret
     
+    # 出力用
     def getOpponentStr(self) -> str:
         ret = ''
         for i in range(3):
             ret += c_char[i+3]*self._captured[i+3]
         return ret
     
+    # 先手が使用可能なPieceを取得する
     def getMyPieces(self) -> list[Piece]:
         return [getPieceNum(capturedIndex) for capturedIndex in range(3) if self._captured[capturedIndex] != 0]
+    
+    # 先手後手を入れ替える
+    def turn(self):
+        new_captured = list(range(RANGE_OF_CAPTURED))
+        for index in range(RANGE_OF_CAPTURED):
+            new_captured[turnIndex(index)] = self._captured[index]
+        return Captured(new_captured)
 
 # 駒を与えると、自分の駒のインデックスを返す
 # 駒取得時・駒使用時の双方から使用される
@@ -135,3 +160,32 @@ capIndexToPieceDic = {
 
 def getPieceNum(index: CapturedIndex) -> Piece:
     return capIndexToPieceDic[index]
+
+turnPieceDic = {
+    EMPTY: EMPTY,
+    MY_ELE_NUM: OP_ELE_NUM,
+    MY_GIR_NUM: OP_GIR_NUM,
+    MY_CHICK_NUM: OP_CHICK_NUM,
+    MY_HEN_NUM: OP_HEN_NUM,
+    MY_LION_NUM: OP_LION_NUM,
+    OP_ELE_NUM: MY_ELE_NUM,
+    OP_GIR_NUM: MY_GIR_NUM,
+    OP_CHICK_NUM: MY_CHICK_NUM,
+    OP_HEN_NUM: MY_HEN_NUM,
+    OP_LION_NUM: MY_LION_NUM
+}
+
+def turnPiece(piece: Piece) -> Piece:
+    return turnPieceDic[piece]
+
+turnIndexDic = {
+    MY_ELE_INDEX: OP_ELE_INDEX,
+    MY_GIR_INDEX: OP_GIR_INDEX,
+    MY_CHICK_INDEX: OP_CHICK_INDEX,
+    OP_ELE_INDEX: MY_ELE_INDEX,
+    OP_GIR_INDEX: MY_GIR_INDEX,
+    OP_CHICK_INDEX: MY_CHICK_INDEX
+}
+
+def turnIndex(index: CapturedIndex) -> CapturedIndex:
+    return turnIndexDic[index]
---
+++ tests/test_turn_board.py
@@ -0,0 +1,60 @@
+from models.board import State
+
+start_board = '''[]
+-------
+|g|l|e|
+-------
+| |c| |
+-------
+| |C| |
+-------
+|E|L|G|
+-------
+[]
+
+'''
+in_progress_board = '''[eg]
+-------
+| |H|l|
+-------
+| | | |
+-------
+|E| | |
+-------
+| |L| |
+-------
+[GC]
+
+'''
+in_progress_board_t = '''[gc]
+-------
+| |l| |
+-------
+| | |e|
+-------
+| | | |
+-------
+|L|h| |
+-------
+[EG]
+
+'''
+
+class TestTurnBoard:
+    # 初期状態
+    def test_initial_board(self):
+        board = [8, 6, 7, 0, 9, 0, 0, 4, 0, 2, 1, 3]
+        captured = [0, 0, 0, 0, 0, 0]
+        state = State.create(board, captured)
+        turn = state.turn()
+        assert state.getStr() == start_board
+        assert turn.getStr() == start_board
+
+    # 手番が進んだ状態
+    def test_in_progress_board(self):
+        board = [0, 5, 6, 0, 0, 0, 2, 0, 0, 0, 1, 0]
+        captured = [0, 1, 1, 1, 1, 0]
+        state = State.create(board, captured)
+        turn = state.turn()
+        assert state.getStr() == in_progress_board
+        assert turn.getStr() == in_progress_board_t
