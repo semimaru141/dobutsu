67396d8d242efa1439c1f2b41f703fae09bee324
semimaru141 <chiharu.ishii@keio.jp>
2023-09-16 22:32:33 +0900

    show_train_distributionを修正

--- src/applications/show_train_data_distribution.py
+++ src/applications/show_train_data_distribution.py
@@ -2,4 +2,4 @@ from src.domains.train_data.train_data_file_factory import TrainDataFileFactory
 
 def show_train_data_distribution(filename: str = 'default'):
     train_data = TrainDataFileFactory(filename).create()
-    print(train_data.get_except_one_appearance_size())
+    print(train_data.show_appearance_distribution())
