1de505036a7f43fed2216eb7f6f813a23a6be1db
semimaru141 <chiharu.ishii@keio.jp>
2023-09-28 15:56:44 +0900

    historyを修正した

--- src/domains/shogi/history.py
+++ src/domains/shogi/history.py
@@ -1,18 +1,19 @@
 from typing import List, Tuple
+from src.consts.domain import Score
 from src.domains.shogi.const import Step
 from src.domains.shogi.shogi_state import ShogiState
 
 class History:
     def __init__(self) -> None:
-        self.list: List[ShogiState] = []
+        self.list: List[Tuple[ShogiState, Step, Score]] = []
 
-    def add_state(self, data: Tuple[ShogiState, Step]) -> None:
+    def data_add(self, data: Tuple[ShogiState, Step, Score]) -> None:
         self.list.append(data)
 
     def __iter__(self) -> 'History':
         return self
     
-    def __next__(self) -> ShogiState:
+    def __next__(self) -> Tuple[ShogiState, Step, Score]:
         if len(self.list) > 0:
             return self.list.pop(0)
         else:
