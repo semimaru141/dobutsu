5a43f5a751e876c1c5a3fab7d55d7dc697813421
semimaru141 <chiharu.ishii@keio.jp>
2023-10-05 16:41:18 +0900

    標準出力を追加

--- src/applications/compress_train_data.py
+++ src/applications/compress_train_data.py
@@ -3,4 +3,5 @@ from src.domains.train_data.train_data_file_factory import TrainDataFileFactory
 def compress_train_data(read_filename: str = 'default', new_filename: str = 'default'):
     train_data = TrainDataFileFactory(read_filename).create()
     compressed_train_data =  train_data.compress()
+    print(f"圧縮後局面数: {compressed_train_data.get_size()}")
     compressed_train_data.save(new_filename)
--- src/applications/train_multi_model.py
+++ src/applications/train_multi_model.py
@@ -9,6 +9,7 @@ from src.domains.train_data.train_data_model_factory import TrainDataModelFactor
 
 LIMIT = 100
 base_key = '060820021000000012'
+target_key = '000600773010012000'
 
 def train_multi_model(trial: int, seed: str, filename: str, model_name: str):
     np.random.seed(seed)
@@ -18,8 +19,12 @@ def train_multi_model(trial: int, seed: str, filename: str, model_name: str):
     for _ in range(trial):
         run(factory, ShogiState.from_key(base_key), 0)
     train_data = factory.create()
+    print("試合回数: 5000")
+    print(f"総出現局面数: {train_data.get_size()}")
+    print(f"2回以上出現した局面数: {train_data.get_except_one_appearance_size()}")
+    print(f"予測回数: {train_data.get_predicted_count()}")
+    print(f"正解局面到達数: {train_data.search_appearance_count(target_key)}")
     train_data.save(filename)
-
 
 def run(factory: TrainDataModelFactory, state: State, step: Step) -> Score:
     # ループからの離脱
